2bc51277d7d4fb46c78ac4010758762a84ac4f09
Bumbac Daniela <bumbacandreeadaniela5@gmail.com>
2023-10-22 22:03:35 +0300

    initial commit

---
+++ Data/Bumbac_Daniela_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Bumbac_Daniela_Lab2.Models;
+
+namespace Bumbac_Daniela_Lab2.Data
+{
+    public class Bumbac_Daniela_Lab2Context : DbContext
+    {
+        public Bumbac_Daniela_Lab2Context (DbContextOptions<Bumbac_Daniela_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bumbac_Daniela_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Bumbac_Daniela_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Bumbac_Daniela_Lab2.Models.Author>? Author { get; set; }
+    }
+}
---
+++ Migrations/20231021191928_Date.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bumbac_Daniela_Lab2.Migrations
+{
+    public partial class Date : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
